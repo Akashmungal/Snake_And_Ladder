b69787ea9b4f97af846046444cc909e4fa21dbfe
Akashmungal <mungalakash92@gmail.com>
2022-03-17 02:07:55 +0530

    Show Start Position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,5 +4,8 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
 
         System.out.println("Welcome To Snake and Ladder Problem");
+
+        int startPosition = 0;
+        System.out.println("Game starts from : " + startPosition);
     }
 }

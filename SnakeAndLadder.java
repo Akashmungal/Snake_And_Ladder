c5fdfdbb6371335b015e6170ac7374e87876f366
Akashmungal <mungalakash92@gmail.com>
2022-03-19 03:40:45 +0530

    Check player Option

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -9,19 +9,40 @@ public class SnakeAndLadder {
         int randomValue = random.nextInt(6) + 1;
         return randomValue;
     }
+
+    static int getRandomOption() {
+        int randomValue1 = random.nextInt(3);
+        return randomValue1;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome To Snake and Ladder Problem");
 
         Scanner in = new Scanner(System.in);
         int startPosition = 0;
+        int currentPosition = 0;
         System.out.println("Game starts from : " + startPosition);
         System.out.println("Enter 1 to roll a die");
         int die = in.nextInt();
-        if(die == 1){
+        if (die == 1) {
             int playerDie = getRandomDieValue();
-            System.out.println("Die Face: " +playerDie);
-        }
-        else {
+            System.out.println("Die Face: " + playerDie);
+            int option = getRandomOption();
+            switch (option) {
+                case 1:
+                    System.out.println("Option: LADDER");
+                    currentPosition += playerDie;
+                    break;
+                case 2:
+                    System.out.println("Option: SNAKE");
+                    currentPosition -= playerDie;
+                    break;
+                default:
+                    System.out.println("Option: NO PLAY");
+                    break;
+                }
+                System.out.println("Current Position: " + currentPosition);
+            }
+            else {
             System.exit(0);
         }
     }
